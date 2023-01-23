01ac083c46cae0cc5c59cb197912f5c2c3fcd30e
Pedro H <pedro.bonel@gmail.com>
2023-01-23 00:29:23 -0300

    Improving syntax highlight

--- lite-xl/plugins/language_tsx.lua
+++ lite-xl/plugins/language_tsx.lua
@@ -2,14 +2,36 @@
 -- Almost identical to JS, with the exception that / shouldn't denote a regex.
 -- Current JS syntax highlighter will highlight half the document due to closing tags.
 local syntax = require "core.syntax"
+local style = require "core.style"
+local core = require "core"
+
+for _, attr in pairs({"bold", "italic", "bold_italic"}) do
+  local attributes = {}
+  if attr ~= "bold_italic" then
+    attributes[attr] = true
+  else
+    attributes["bold"] = true
+    attributes["italic"] = true
+  end
+  -- no way to copy user custom font with additional attributes :(
+  style.syntax_fonts["tsx_"..attr] = renderer.font.load(
+    USERDIR .. "/fonts/Fira-Code-Medium.ttf",
+    style.code_font:get_size(),
+    attributes
+  )
+  -- also add a color for it
+  style.syntax["tsx_"..attr] = style.syntax["keyword2"]
+end
+
 
 syntax.add {
   name = "TypeScript with JSX",
   files = { "%.tsx$" },
+  block_comment = { "{/*", "*/}" },
   comment = "//",
   patterns = {
     { pattern = "//.-\n",               type = "comment"  },
-    { pattern = { "/%*", "%*/" },       type = "comment"  },
+    { pattern = { "{/%*", "%*/}" },       type = "comment"  },
     { pattern = { '"', '"', '\\' },     type = "string"   },
     { pattern = { "'", "'", '\\' },     type = "string"   },
     { pattern = { "`", "`", '\\' },     type = "string"   },
@@ -17,14 +39,14 @@ syntax.add {
     { pattern = "-?%d+[%d%.eE]*",       type = "number"   },
     { pattern = "-?%.?%d+",             type = "number"   },
     { pattern = "[%+%-=/%*%^%%<>!~|&]", type = "operator" },
-    { pattern = "interface%s()[%a_][%w_]*", type = {"keyword", "keyword2"} },
-    { pattern = "type%s()[%a_][%w_]*", type = {"keyword", "keyword2"} },
+    { pattern = "interface%s()[%a_][%w_]*", type = {"keyword", "tsx_bold"} },
+    { pattern = "type%s()[%a_][%w_]*", type = {"keyword", "tsx_bold_italic"} },
     { pattern = "[%a_][%w_]*%f[(]",     type = "function" },
     -- { pattern = "[%a_][%w_]*",          type = "symbol"   },
     { pattern = "%f[^<]![%a_][%w_]*",      type = "keyword2" },
     { pattern = "%f[^<][%a_][%w_]*",       type = "function" },
     { pattern = "%f[^</][%a_][%w_]*",      type = "function" },
-    { pattern = "%s*%a*[=]",             type = "keyword"  },
+    { pattern = "%s*%a*[=]",             type = "keyword2"  },
   },
   symbols = {
     ["async"]      = "keyword",
