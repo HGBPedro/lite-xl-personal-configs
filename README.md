3c076c6a06dcbc0018228161580d15012ff57861
Pedro H G Bonel <pedro.bonel@gmail.com>
2023-04-01 14:26:49 -0300

    Changes on script and readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # lite-xl-personal-configs
 Lite-XL personal configs. Will be rearranged soon.
+
+Needed packages:
+meson
+ninja
+cmake
+lua
--- autoinstall.sh
+++ autoinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd
+if [ ! -d "~/Downloads" ]; then mkdir Downloads; fi
 cd Downloads
-mkdir teste
-git clone git@github.com:lite-xl/lite-xl.git
+git clone https://github.com/lite-xl/lite-xl.git
 cd lite-xl
 bash build-packages.sh
 mkdir -p $HOME/.local/bin && cp bin/lite-xl $HOME/.local/bin
@@ -13,7 +13,7 @@ xdg-desktop-menu forceupdate
 
 cd
 cd Downloads
-git clone git@github.com:HGBPedro/lite-xl-personal-configs.git
+git clone https://github.com/HGBPedro/lite-xl-personal-configs.git
 cd lite-xl-personal-configs
 cp lite-xl/fonts/fonts ~/.config/lite-xl
 cp lite-xl/colors/silmarillion.lua ~/.config/lite-xl/colors
