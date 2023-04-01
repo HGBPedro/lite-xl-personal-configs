141ee5111b221af9c8bc120e5399d2022c1521f6
Pedro H G Bonel <pedro.bonel@gmail.com>
2023-04-01 13:45:11 -0300

    Initial script

---
+++ autoinstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd
+cd Downloads
+mkdir teste
+git clone git@github.com:lite-xl/lite-xl.git
+cd lite-xl
+bash build-packages.sh
+mkdir -p $HOME/.local/bin && cp bin/lite-xl $HOME/.local/bin
+cp -r share $HOME/.local
+echo -e 'export PATH=$PATH:$HOME/.local/bin' >> $HOME/.bashrc
+xdg-desktop-menu forceupdate
+
+cd
+cd Downloads
+git clone git@github.com:HGBPedro/lite-xl-personal-configs.git
+cd lite-xl-personal-configs
+cp lite-xl/fonts/fonts ~/.config/lite-xl
+cp lite-xl/colors/silmarillion.lua ~/.config/lite-xl/colors
+
